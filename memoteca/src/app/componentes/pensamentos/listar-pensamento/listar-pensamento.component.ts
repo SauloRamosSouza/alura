7558f95b47765396011030a7b3f8a36a6e57810a
SauloRamosSouza <saulosud@gmail.com>
2024-03-21 19:35:51 -0300

    Implementacao de interface Pensamento

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -6,36 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent {
-  listaPensamentos = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input',
-      autoria: 'Componente filho',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'Comunicação entre componentes',
-      autoria: 'Angular',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'A felicidade de sua vida depende da qualidade de seus pensamentos.',
-      autoria: 'Marco Aurélio',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'Tente mover o mundo - o primeiro passo será mover a si mesmo.',
-      autoria: 'Platão',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem mollitia a repe.Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem mollitia a repe.Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem mollitia a repe.',
-      autoria: '',
-      modelo: 'modelo1'
-    },
-  ];
+  listaPensamentos:Pensamento[] = [];
 }
