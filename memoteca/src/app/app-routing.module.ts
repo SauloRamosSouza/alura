3de7c3276b7136076c867bb6b08ea1c697641128
SauloRamosSouza <saulosud@gmail.com>
2024-03-22 16:30:02 -0300

    Implementaçao de medotos criar, buscrPorId, excluir

--- memoteca/src/app/app-routing.module.ts
+++ memoteca/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensam
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'listarPensamento', pathMatch: 'full'},
   {path: 'criarPensamento', component: CriarPensamentoComponent},
-  {path: 'listarPensamento', component: ListarPensamentoComponent}
+  {path: 'listarPensamento', component: ListarPensamentoComponent},
+  {path: 'pensamentos/excluirPensamento/:id', component: ExcluirPensamentoComponent}
 ];
 
 @NgModule({
--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { PensamentoService } from '../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component } from '@angular/core';
 
@@ -8,17 +10,23 @@ import { Component } from '@angular/core';
 })
 export class CriarPensamentoComponent {
     pensamento: Pensamento = {
-      id:1,
-      conteudo: 'Aprendendo Angular',
-      autoria: 'Dev',
+      conteudo: '',
+      autoria: '',
       modelo: 'modelo1'
     }
 
+    constructor(
+      private service: PensamentoService,
+      private router: Router
+      ) {}
+
     criarPensamento(){
-      alert("Pensamento criado com sucesso!")
+      this.service.criar(this.pensamento).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      });
     }
 
     excluirPensamento(){
-      alert("Pensamento excluido com sucesso!")
+      this.router.navigate(['/listarPensamento'])
     }
 }
